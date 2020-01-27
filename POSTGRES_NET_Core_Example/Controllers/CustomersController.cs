7213556264d4ed61c27789d67755a02bd8443c9b
Shan <sskset@gmail.com>
2020-01-27 20:32:05 +1100

    First commit

---
+++ POSTGRES_NET_Core_Example/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using POSTGRES_NET_Core_Example.Database;
+using POSTGRES_NET_Core_Example.Models;
+
+namespace POSTGRES_NET_Core_Example.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomersController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository ?? throw new ArgumentNullException(nameof(customerRepository));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Craate(Customer payload)
+        {
+            var result = await _customerRepository.CreateAsync(payload.FirstName, payload.LastName);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _customerRepository.GetAsync(id);
+            return Ok(result);
+        }
+    }
+}
---
+++ POSTGRES_NET_Core_Example/Database/CustomerRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using POSTGRES_NET_Core_Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Npgsql;
+
+namespace POSTGRES_NET_Core_Example.Database
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer> CreateAsync(string firstName, string lastName);
+
+        Task<Customer> GetAsync(int id);
+    }
+
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly IConfiguration _configuration;
+        public string ConnectionString { get; }
+
+        public CustomerRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            ConnectionString = _configuration.GetConnectionString("CustomerDb");
+        }
+
+        public async Task<Customer> CreateAsync(string firstName, string lastName)
+        {
+            var sql = "INSERT INTO public.\"Customers\" (firstname, lastname) values (@firstName, @lastName) returning id;";
+
+            using(var conn = new Npgsql.NpgsqlConnection(ConnectionString))
+            {
+                var id = await conn.ExecuteScalarAsync<int>(sql, new { firstName, lastName });
+
+                return await GetAsync(id);
+            }
+        }
+
+        public async Task<Customer> GetAsync(int id)
+        {
+            var sql = "select * from public.\"Customers\" where id=@id";
+
+            using (var conn = new NpgsqlConnection(ConnectionString))
+            {
+                return await conn.QuerySingleOrDefaultAsync<Customer>(sql, new { id });
+            }
+        }
+    }
+}
